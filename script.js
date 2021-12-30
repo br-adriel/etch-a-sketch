8600bfc2c294b643a8100f8df57cdf9919ef8c8d
Adriel <adriel.fsantos@outlook.com>
2021-12-30 13:40:53 -0300

    Implementa apagarDisplay e novoDisplay

--- script.js
+++ script.js
@@ -3,15 +3,25 @@ let currentBrush = document.getElementsByClassName("selecionado")[0];
 let gridSize = 16;
 let currentColor = currentBrush.dataset.color;
 
-display.style.cssText = "grid-template-columns: repeat(16, 1fr);";
-
-for (let i = 0; i < gridSize; i++) {
-  for (let j = 0; j < gridSize; j++) {
-    let pixel = document.createElement("div");
-    pixel.classList.add("pixel");
-    pixel.addEventListener("mouseenter", () => {
-      pixel.style.background = currentColor;
-    });
-    display.appendChild(pixel);
+function novoDisplay(gridSize, color) {
+  display.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr);`;
+  apagarDisplay();
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      let pixel = document.createElement("div");
+      pixel.classList.add("pixel");
+      pixel.addEventListener("mouseenter", () => {
+        pixel.style.background = color;
+      });
+      display.appendChild(pixel);
+    }
   }
 }
+
+function apagarDisplay() {
+  while (display.lastChild) {
+    display.removeChild(display.lastChild);
+  }
+}
+
+novoDisplay(gridSize, currentColor);
